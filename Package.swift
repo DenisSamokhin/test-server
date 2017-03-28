c7bb99cdbe5a0a8fd534c08dce06ebca116a92b0
Denis Samokhin <denis.samokhin2011@gmail.com>
2017-03-28 12:56:17 +0300

    Implement PostgreSQL database

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "test-server",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0)
     ],
     exclude: [
