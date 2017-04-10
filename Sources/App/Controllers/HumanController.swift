d4b02688ce9afd4c1b2208afb720d9a95aa2f47a
Denis Samokhin <denis.samokhin2011@gmail.com>
2017-04-10 13:45:31 +0300

    Implement image save

--- Sources/App/Controllers/HumanController.swift
+++ Sources/App/Controllers/HumanController.swift
@@ -25,11 +25,12 @@ final class HumanController {
             guard let name = request.data["fullname"]?.string, let age = request.data["age"]?.int, let email = request.data["email"]?.string, let image = request.data["image"] else {
                     throw Abort.custom(status: .badRequest, message: "Some parameters are missed.")
             }
+            try request.saveImage()
             var person = Human(name: name, email: email, age: age)
             try person.save()
             return try person.makeJSON()
         }else {
-            return "Wrong Content-Type. Must be \"application/json\""
+            throw Abort.custom(status: .badRequest, message: "Wrong Content-Type. Must be \"multipart/form-data\"")
         }
     }
 
@@ -41,6 +42,27 @@ extension Request {
             return true
         }else {
             return false
+        }
+    }
+    
+    func saveImage() throws {
+        guard let fileData = self.formData?["image"]?.part.body, let fileName = self.formData?["fileName"]?.string else {
+            throw Abort.custom(status: .badRequest, message: "No file in request")
+        }
+        
+//        guard  else {
+//            throw Abort.custom(status: .internalServerError, message: "Missing working directory")
+//        }
+        let workPath = Droplet().workDir
+        let name = UUID().uuidString + "_" + fileName + ".png"
+        let imageFolder = "Public/images/event-images"
+        let saveURL = URL(fileURLWithPath: workPath).appendingPathComponent(imageFolder, isDirectory: true).appendingPathComponent(name, isDirectory: false)
+        
+        do {
+            let data = Data(bytes: fileData)
+            try data.write(to: saveURL)
+        } catch {
+            throw Abort.custom(status: .internalServerError, message: "Unable to write multipart form data to file. Underlying error \(error)")
         }
     }
 }
